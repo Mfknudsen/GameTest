541c2e8cc08f0214b4ec1e69666041bd88c11db4
Mfknudsen <35879356+Mfknudsen@users.noreply.github.com>
2019-05-26 13:55:00 +0200

    +

---
+++ Galactic War/Assets/Overall/Commander/Minimap_Commander.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap_Commander : MonoBehaviour
+{
+
+    float MoveX, MoveZ;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
